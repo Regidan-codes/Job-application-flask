c51d139c314cc32a623df30efae81af285e6d3a7
Regidan <regidancodes@gmail.com>
2023-12-12 23:12:40 +0530

    Storing data in database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -8,11 +9,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
 
-class Form(db.model):
+class Form(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
-    email = db.Column(db.string(80), unique=True)
+    email = db.Column(db.String(80), unique=True)
     date = db.Column(db.Date)
     occupation = db.Column(db.String(80))
 
@@ -24,7 +25,12 @@ def index():
         last_name = request.form["last_name"]
         email = request.form["email"]
         date = request.form["date"]
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
         occupation = request.form["occupation"]
+        form = Form(first_name=first_name, last_name=last_name,
+                    email=email, date=date_obj, occupation=occupation)
+        db.session.add(form)
+        db.session.commit()
 
     return render_template("index.html")
 
